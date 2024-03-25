b0234e5164e209fb32085582362c2d5054fe5bae
Chavan Pranit <chavapra@ap.dtcs.local>
2024-03-25 11:49:14 +0530

    Log changes

--- src/services/uploadChunks.ts
+++ src/services/uploadChunks.ts
@@ -15,7 +15,7 @@ export default async function uploadChunksToStorage(sessionId: string): Promise<
   //  Read the list of files in the directory
   const files: string[] = fs.readdirSync(chunksDir);
 
-  logger.info('Upload Process', { information: `Upload process started for id: ${sessionId}`, Timestamp: formatDate(new Date()) });
+  logger.info('Upload Process', { Information: `Upload process started for id: ${sessionId}`, Timestamp: formatDate(new Date()) });
 
   for (const fileName of files) {
     const filePath = path.join(chunksDir, fileName);
@@ -28,7 +28,7 @@ export default async function uploadChunksToStorage(sessionId: string): Promise<
     });
 
     if (error) {
-      logger.error('Upload Process', { information: `Upload process failed for id: ${sessionId} : ${error.message}`, Timestamp: formatDate(new Date()) });
+      logger.error('Upload Process', { Information: `Upload process failed for id: ${sessionId} : ${error.message}`, Timestamp: formatDate(new Date()) });
       throw new SupabaseStorageExceptions(`FAILED TO UPLOAD VIDEO CHUNKS : ${error.message}`);
     }
   }
